57abbdc1d7ce8bedfe68509b573b302fb2ce6fb8
endeavormoquan <moquan@mail.nwpu.edu.cn>
2020-01-12 10:39:13 +0800

    update freeze function for vgg

--- ptseg/utils/freeze.py
+++ ptseg/utils/freeze.py
@@ -28,7 +28,22 @@ def free_resnet50(model):
 
 
 def free_vgg(model):
-    for name, param in model.classifier.named_parameters():
+    """
+    (classifier): Sequential(
+    (0): Linear(in_features=25088, out_features=4096, bias=True)
+    (1): ReLU(inplace)
+    (2): Dropout(p=0.5)
+    (3): Linear(in_features=4096, out_features=4096, bias=True)
+    (4): ReLU(inplace)
+    (5): Dropout(p=0.5)
+    (6): Linear(in_features=4096, out_features=1000, bias=True)
+    )
+    """
+    # for name, param in model.classifier.named_parameters():
+    #     param.requires_grad = True
+    for name, param in model.classifier[3].named_parameters():
+        param.requires_grad = True
+    for name, param in model.classifier[6].named_parameters():
         param.requires_grad = True
     return model
 
